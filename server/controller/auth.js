71e5b28d5b6605b1c752fe80133b227ce72e1da4
gnoluv <vulongpt@gmail.com>
2022-11-28 23:10:14 +0700

    add some files

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,49 +1,48 @@
-const User = require('../models/auth')
-const jwt = require('jsonwebtoken')
+const User = require("../models/auth");
+const jwt = require("jsonwebtoken");
 
-
-const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn: '3h'})
-}
+const createToken = _id => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3h" });
+};
 // login user
 const loginUser = async (req, res) => {
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
   try {
-    const user = await User.login(email, password)
+    const user = await User.login(email, password);
 
     // create a token
-    const token = createToken(user._id)
-    res.cookie('t', token, { expire: new Date()+ 9999})
-    res.status(200).json({email, token})
+    const token = createToken(user._id);
+    res.cookie("t", token, { expire: new Date() + 9999 });
+    res.status(200).json({ email, token });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-} 
+};
 
 // signup user
-const signUpUser = async (req, res) =>{
-  const received = req.body
+const signUpUser = async (req, res) => {
+  const received = req.body;
   try {
-    const user = await User.signup(received)
-    
+    const user = await User.signup(received);
+
     //create a token
-    const token = createToken(user._id)
-    res.status(200).json({ message:"Register Success", token})
+    const token = createToken(user._id);
+    res.status(200).json({ message: "Register Success", token });
   } catch (error) {
-    res.status(400).json({ error: error.message})
+    res.status(400).json({ error: error.message });
   }
   res.json({
-    message: 'signup user'
-  })
-}
+    message: "signup user",
+  });
+};
 
-export const logout = async (req, res) => {
-  res.clearCookie('t');
+const logout = async (req, res) => {
+  res.clearCookie("t");
   //  res.redirect('/signin');
   res.json({
-      message: "Signout Success"
-  })
-}
+    message: "Signout Success",
+  });
+};
 
-module.exports = { signUpUser, loginUser}
+module.exports = { signUpUser, loginUser, logout };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require("express");
 
 // controller Function
-const {loginUser, signUpUser} = require('../controller/auth')
+const { loginUser, signUpUser, logout } = require("../controller/auth");
 const router = express.Router();
 
 // sign up route
-router.post('/signup', signUpUser);
+router.post("/signup", signUpUser);
 
 // login route
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 
 // sign out route
-router.get('/signout', logout)
+router.get("/signout", logout);
 module.exports = router;
