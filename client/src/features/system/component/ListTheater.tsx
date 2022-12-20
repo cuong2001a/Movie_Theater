6fdb25fca1d0327b7b5f22e1595bac6af41a44e6
cuongnv <cuongnv.fpl@gmail.com>
2022-12-20 11:13:27 +0700

    style list Location

--- client/src/features/system/component/ListTheater.tsx
+++ client/src/features/system/component/ListTheater.tsx
@@ -2,11 +2,25 @@ import React from "react";
 import classes from "./style.module.scss";
 import { IoLocationSharp } from "react-icons/io5";
 import { IItemTheater, LIST_THREATER } from "../constant";
+import { ImLocation2 } from "react-icons/im";
 const ListTheater: React.FC = () => {
   const city = ["Thành phố Hồ Chí Minh", "Hà Nội", "Hải Phòng", "Huế"];
+
+  const renderCity = () => {
+    return (
+      <>
+        {city?.map((item, index) => (
+          <div className={classes.item_theater}>
+            <ImLocation2 />
+            <span>{item}</span>
+          </div>
+        ))}
+      </>
+    );
+  };
   return (
     <React.Fragment>
-      <div>List Threater</div>
+      <div className={classes.list_theater}>{renderCity()}</div>
     </React.Fragment>
   );
 };
