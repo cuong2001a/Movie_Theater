2b7ad9c490a42cc8360e304524f92612fb608921
cuongnv <cuongnv.fpl@gmail.com>
2023-01-30 14:50:00 +0700

    edit UI Home and menu

--- client/src/features/home/components/CurrentMovie.tsx
+++ client/src/features/home/components/CurrentMovie.tsx
@@ -1,12 +1,62 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useCallback, useEffect, useState } from "react";
 import classes from "../home.module.scss";
 import Slider from "react-slick";
 import { GrPrevious, GrNext } from "react-icons/gr";
 import { Link } from "react-router-dom";
 import { SettingSlickMovie } from "../../../constant/models";
 
+interface TypeItemMovie {
+  id: number;
+  name: string;
+  dateTo: string;
+  dateFrom: string;
+  images: string;
+}
+const data = [
+  {
+    id: 0,
+    name: "Doraemon 2 : STAND BY ME",
+    dateTo: "09/03/2022",
+    dateFrom: "28/08/2022",
+    images:
+      "https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg",
+  },
+  {
+    id: 1,
+    name: "Doraemon 2 : STAND BY ME 1",
+    dateTo: "09/03/2022",
+    dateFrom: "28/08/2022",
+    images:
+      "https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg",
+  },
+  {
+    id: 2,
+    name: "Doraemon 2 : STAND BY ME 2",
+    dateTo: "09/03/2022",
+    dateFrom: "28/08/2022",
+    images:
+      "https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg",
+  },
+  {
+    id: 3,
+    name: "Doraemon 2 : STAND BY ME 3",
+    dateTo: "09/03/2022",
+    dateFrom: "28/08/2022",
+    images:
+      "https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg",
+  },
+  {
+    id: 4,
+    name: "Doraemon 2 : STAND BY ME 4",
+    dateTo: "09/03/2022",
+    dateFrom: "28/08/2022",
+    images:
+      "https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg",
+  },
+];
 const CurrentMovie = () => {
   const [isHover, setIsHover] = useState<Boolean>(false);
+  const [indexActive, setIndexActive] = useState<number | undefined>();
   const setting: SettingSlickMovie = {
     infinite: true,
     speed: 500,
@@ -15,54 +65,49 @@ const CurrentMovie = () => {
     nextArrow: <GrNext />,
     prevArrow: <GrPrevious />,
   };
+
+  useEffect(() => {
+    if (indexActive || indexActive == 0) {
+      setIsHover(true);
+    } else {
+      setIsHover(false);
+    }
+  }, [indexActive]);
+
   return (
     <React.Fragment>
       <div className={classes.currentMovie}>
         <span className={classes.title}>-- Phim hiện đang chiếu --</span>
         <Slider {...setting}>
-          <div
-            className={classes.item}
-            onMouseOver={() => setIsHover(true)}
-            onMouseOut={() => setIsHover(false)}
-          >
-            <img
-              src="https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg"
-              alt=""
-            />
-            <div className={isHover ? classes.info_hover : classes.info}>
-              <h3 className={classes.info__title}>Doraemon 2 : STAND BY ME</h3>
-              <p className={classes.info__time}>từ 09/03/2022 đến 28/08/2022</p>
-            </div>
-            <div className={classes.order}>
-              <Link to={"/"} className={classes.link}>
-                Mua vé
-              </Link>
-            </div>
-          </div>
-          <div className={classes.item}>
-            <img
-              src="https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg"
-              alt=""
-            />
-          </div>
-          <div className={classes.item}>
-            <img
-              src="https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg"
-              alt=""
-            />
-          </div>
-          <div className={classes.item}>
-            <img
-              src="https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg"
-              alt=""
-            />
-          </div>
-          <div className={classes.item}>
-            <img
-              src="https://res.cloudinary.com/cgv-vi-t-nam/image/upload/v1640168025/poster_movie/xxlsvvjzr5bn7zza5xy6.jpg"
-              alt=""
-            />
-          </div>
+          {data?.map((item: TypeItemMovie, index: number) => {
+            return (
+              <div
+                key={index}
+                className={classes.item}
+                onMouseOver={() => setIndexActive(index)}
+                onMouseOut={() => setIndexActive(undefined)}
+              >
+                <img src={item.images} alt="Thông tin hình ảnh" />
+                <div
+                  className={
+                    isHover && indexActive == index
+                      ? classes.info_hover
+                      : classes.info
+                  }
+                >
+                  <h3 className={classes.info__title}>{item.name}</h3>
+                  <p className={classes.info__time}>
+                    từ {item.dateFrom} đến {item.dateTo}
+                  </p>
+                </div>
+                <div className={classes.order}>
+                  <Link to={"/"} className={classes.link}>
+                    Mua vé
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
         </Slider>
       </div>
     </React.Fragment>
